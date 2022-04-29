f84cb30b15ba6b8c8a05fe1928abe15d32c79912
so <S.phairoh20dec@gmail.com>
2022-04-29 21:49:01 +0700

    reset vue!!

--- FE/BookingProject/src/App.vue
+++ FE/BookingProject/src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
-
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
- 
-<template>
-<div>
-    <div>
-      <h1>hi</h1>
-      <router-link :to = "{ name: 'Home' }">Home</router-link> |       
-      <router-link :to = "{ name: 'AllList' }">show list</router-link> 
-    </div>
-    <div>
-      <router-view></router-view>
-    </div>
-</div>
-</template>
- 
-<style>
 
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- FE/BookingProject/src/main.js
+++ FE/BookingProject/src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import './input.css'
 
-const app = createApp(App)
-app.use(router)    
-app.mount('#app')
+createApp(App).mount('#app')
