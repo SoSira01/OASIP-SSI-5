e531055073dc1e70ae4af72ab5e0a3eb33663d7b
so_sira <47183080+SoSira01@users.noreply.github.com>
2022-05-02 12:22:55 +0700

    working

--- BE/src/main/java/com/example/booking/controller/BookingController.java
+++ BE/src/main/java/com/example/booking/controller/BookingController.java
@@ -1,11 +1,9 @@
 package com.example.booking.controller;
 
-import com.example.booking.dtos.AllBookingDTO;
 import com.example.booking.dtos.BookingDTO;
 import com.example.booking.entities.Booking;
 import com.example.booking.services.BookingService;
 import org.springframework.web.bind.annotation.*;
-import com.example.booking.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -13,32 +11,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/booking")
 public class BookingController {
-    @Autowired
-    private BookingRepository repository;
     //get all working
-//    @GetMapping("")
-//    public Page<Booking> getAllBookings(
-//            @RequestParam(defaultValue = "id") String sortBy,
-//            @RequestParam(defaultValue = "0") Integer page,
-//            @RequestParam(defaultValue = "5") Integer pageSize) {
-//        Sort sort = Sort.by(sortBy);
-//        return repository.findAll(PageRequest.of(page, pageSize, sort));
-//    }
     @GetMapping("")
-    public List<BookingDTO> getBookingById(){
+    public List<BookingDTO> getAllBooking(){
         return  bookingservices.getBooking();
     }
     @Autowired
     private BookingService bookingservices;
     //get by id working
     @GetMapping("/{id}")
-    public AllBookingDTO getBookingById(@PathVariable Integer id){
+    public BookingDTO getBookingById(@PathVariable Integer id){
         return  bookingservices.getBookingById(id);
     }
-    //create booking not working
+    //create booking
     @PostMapping("")
     public Booking create(@RequestBody BookingDTO newBooking){
-        return bookingservices.save (newBooking);
+        return bookingservices.create(newBooking);
     }
 
     @DeleteMapping("/{id}")
